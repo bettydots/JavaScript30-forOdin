caea73daee5727054ac5293693f48932c5ed1622
bettydots <93158166+bettydots@users.noreply.github.com>
2025-09-17 19:27:01 -0500

    Finished exercise 03
    No changes from original tutorial, possible solution to smooth
    color change in YT comments

---
+++ 03 - CSS Variables/script.js
@@ -0,0 +1,14 @@
+const inputs = document.querySelectorAll(".controls input");
+const img = document.querySelectorAll("img");
+
+function handleUpdate() {
+  // console.log(this.value);
+  const suffix = this.dataset.sizing || "";
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    `${this.value}${suffix}`,
+  );
+}
+
+inputs.forEach((input) => input.addEventListener("change", handleUpdate));
+inputs.forEach((input) => input.addEventListener("mousemove", handleUpdate));
